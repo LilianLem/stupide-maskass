918b6810bb3dd64eb569537dcb64caa5bd6db5f6
Caropox <54894433+Caropox@users.noreply.github.com>
2019-12-18 16:20:59 +0100

    tableau points

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 export function checkGame(){
     let pointsArray = [3, 5, 4];
-    
-}   
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let pointsToWin = pointsArray.reduce(reducer);
+}
