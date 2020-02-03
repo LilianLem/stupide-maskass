64c148fe3776776a30e57c9f17f581ccede8a762
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-02-03 23:10:30 +0100

    Ajout d'un state non défini (cf fonctions commit précédent)

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ export default class App extends React.Component {
             drawValue2: 0,
             isHandLocked: true,
             isHandDisplayed: false,
-            isPlayerPointsDisplayed: false
+            isPlayerPointsDisplayed: false,
+            displayPlayedCards: false
 		};
 	}
 
